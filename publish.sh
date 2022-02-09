425d5e337b4aaedaaceeed4087daa8695cb26fb2
joshua.software.dev <joshua.software.dev@gmail.com>
2022-02-09 13:41:18 -0800

    Add github action for making release.

--- publish.sh
+++ publish.sh
@@ -5,3 +5,5 @@ SCRIPTPATH=$(dirname "$SCRIPT")
 cd "$SCRIPTPATH"
 
 dotnet publish -c Release
+PLUGIN_VERSION=$(python -c "import xml.etree.ElementTree as ET;print(ET.parse('Version/Version.csproj').getroot()[0][0].text)")
+echo "::set-output name=plugin_version::${PLUGIN_VERSION}"
