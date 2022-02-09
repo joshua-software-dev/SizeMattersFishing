f709ea02f62a09b314b5f95f6aead71d1c91359b
joshua.software.dev <joshua.software.dev@gmail.com>
2022-02-09 01:06:22 -0800

    Fix dev script

--- dev_publish.sh
+++ dev_publish.sh
@@ -4,5 +4,4 @@ SCRIPT=$(readlink -f "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 cd "$SCRIPTPATH"
 
-#./publish.sh && 
-ssh windows "%APPDATA%\XIVLauncher\liveDevPlugins\size.bat"
+./publish.sh && ssh windows "%APPDATA%\XIVLauncher\liveDevPlugins\size.bat"
